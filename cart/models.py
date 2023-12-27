73e5bc464217693fbf4bbe07df96b5f45c14f359
stormshadow47 <69762749+stormshadow47@users.noreply.github.com>
2023-12-27 19:36:57 +0530

    Update models.py

--- cart/models.py
+++ cart/models.py
@@ -13,4 +13,4 @@ class CartItem(models.Model):
     quantity = models.PositiveIntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-# Create your models here.
+
